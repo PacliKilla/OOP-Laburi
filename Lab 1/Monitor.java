60db45d05a905f472fbe760850811a1753724fa0
Pavel Cerlat <64590513+PacliKilla@users.noreply.github.com>
2020-12-17 10:35:44 +0200

    Add files via upload

---
+++ Lab 1/Monitor.java
@@ -0,0 +1,12 @@
+package com.company.lab;
+
+public class Monitor {
+    String color = "Blue";
+    float height = 39.2f;
+    float depth = 12.8f;
+    float width = 48.5f;
+    String resolution = "1900x1090";
+
+
+}
+
